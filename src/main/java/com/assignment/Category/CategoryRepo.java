092f043e724864155eec5f910f29baf9c76c119e
Prachi Sharma <kavyapsharma17@gmail.com>
2022-06-05 22:24:35 +0530

    CRUD

---
+++ src/main/java/com/assignment/Category/CategoryRepo.java
@@ -0,0 +1,8 @@
+package com.assignment.Category;
+
+import com.assignment.Product.JpaRepository;
+import com.assignment.Product.Product;
+
+public interface CategoryRepo extends JpaRepository<Product, Integer> {
+
+}
